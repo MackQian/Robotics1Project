5c2fbbb6e2807892b388db135f90ea5c153515e1
Mack Qian <qianm@rpi.edu>
2020-10-18 16:41:39 -0400

    syntax error update on the controller

--- RoboticsProject/controllers/cameraTest/cameraTest.py
+++ RoboticsProject/controllers/cameraTest/cameraTest.py
@@ -23,11 +23,9 @@ while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-     x=cam.getImage()
+    x=cam.getImage()
     # Process sensor data here.
-
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
     pass
-
 # Enter here exit cleanup code.
