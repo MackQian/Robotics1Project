14afc00df2eb7423e67edbadbe2c8c7d7ca1ec44
sivwof89 <73030438+sivwof89@users.noreply.github.com>
2020-10-25 14:36:58 -0700

    Moved some files around

--- RoboticsProject/controllers/movementTest/build/release/movementTest.d
+++ RoboticsProject/controllers/movementTest/build/release/movementTest.d
@@ -1,3 +1,4 @@
 build/release/movementTest.o: movementTest.cpp \
  C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/cpp/webots/Keyboard.hpp
+ C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ base.h gripper.h
