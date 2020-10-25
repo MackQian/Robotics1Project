8dea0d1d48945969d28442e2c5b1df270e2ad160
sivwof89 <73030438+sivwof89@users.noreply.github.com>
2020-10-25 14:19:27 -0700

    Made a new controller to test movments.

---
+++ RoboticsProject/controllers/movementTest/build/release/movementTest.d
@@ -0,0 +1,3 @@
+build/release/movementTest.o: movementTest.cpp \
+ C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/cpp/webots/Keyboard.hpp
