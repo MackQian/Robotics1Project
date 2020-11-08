4b9325bf79eb55e395b4eacf2edda078eea7431d
hancocktony <hancoa@rpi.edu>
2020-11-08 13:07:30 -0500

    Fixed warning in ArmTest.py

--- RoboticsProject/controllers/movementTest/build/release/movementTest.d
+++ RoboticsProject/controllers/movementTest/build/release/movementTest.d
@@ -1,13 +1,13 @@
 build/release/movementTest.o: movementTest.c \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
  include/arm.h src/arm.c \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
  include/base.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
  src/base.c include/tiny_math.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
- C:\Users\mf588\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\Tony\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
  include/gripper.h src/gripper.c src/tiny_math.c
